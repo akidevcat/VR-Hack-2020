f5021f38b7144ef3f5d1a2bfa7d651bfe1b1eb77
Satsuru <38999255+wolfniey@users.noreply.github.com>
2020-02-17 12:46:48 +0300

    Sync

--- Assets/InterNext/InterNextCore.cs
+++ Assets/InterNext/InterNextCore.cs
@@ -38,8 +38,10 @@ public struct INDevice
 
 public class InterNextCore : MonoBehaviour
 {
+    public static bool PowerDevice = false;
     public string CurrentDevice = "AsyncEngine";
     private INDevice LoadedDevice;
+    private Vector3[] DetailsRealOffsets;
     private GameObject InstancedDevice;
     private Transform InstancedDeviceT;
     private bool disassembled = false;
@@ -53,17 +55,54 @@ public class InterNextCore : MonoBehaviour
     {
         string json = Resources.Load<TextAsset>($"Devices/{name}/common").text;
         LoadedDevice = JsonUtility.FromJson<INDevice>(json);
-        Debug.Log(LoadedDevice.name);
         var prefab = Resources.Load<GameObject>($"Devices/{name}/prefab");
         InstancedDevice = Instantiate(prefab);
         InstancedDeviceT = InstancedDevice.transform;
         InstancedDeviceT.rotation = LoadedDevice.Rotation;
         InstancedDeviceT.position = LoadedDevice.PivotOffset;
         InstancedDeviceT.localScale = LoadedDevice.Scale;
+
+        Transform[] children = new Transform[InstancedDeviceT.childCount];
+        for (int i = 0; i < InstancedDeviceT.childCount; i++)
+            children[i] = InstancedDeviceT.GetChild(i);
+
+        DetailsRealOffsets = new Vector3[children.Length];
+
+        int index = 0;
+        for (int i = 0; i < children.Length; i++)
+        {
+            var child = children[i];
+
+            if (!child.CompareTag("DeviceIgnore") && !child.CompareTag("DetailPivot"))
+            {
+                var pivot = new GameObject($"Detail {index}");
+                pivot.transform.position = child.gameObject.GetComponent<MeshRenderer>().bounds.center + InstancedDeviceT.position;
+                pivot.transform.SetParent(InstancedDeviceT, true);
+                child.SetParent(pivot.transform, true);
+                pivot.tag = "DetailPivot";
+                var spin = pivot.AddComponent<InterNextSpin>();
+                spin.Axis = LoadedDevice.rotation_axis[index];
+                spin.Speed = 3f;
+                DetailsRealOffsets[index] = pivot.transform.localPosition;
+                index++;
+            }
+        }
+    }
+
+    IEnumerator LerpMove(Transform t, Vector3 target, Vector3 start, float time, float startTime)
+    {
+        while (Time.time < startTime + time)
+        {
+            float tm = (Time.time - startTime) / time;
+            t.localPosition = Vector3.Lerp(start, target, Mathf.SmoothStep(0, 1, tm));
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+
     }
 
     public void Disassemble()
     {
+        StopAllCoroutines();
         if (!disassembled)
         {
             int index = 0;
@@ -73,9 +112,12 @@ public class InterNextCore : MonoBehaviour
                 
                 if (!child.CompareTag("DeviceIgnore"))
                 {
-                    child.localPosition = new Vector3(LoadedDevice.details_offsets[index * 3],
+                    //child.localPosition += new Vector3(LoadedDevice.details_offsets[index * 3],
+                    //                                  LoadedDevice.details_offsets[index * 3 + 1],
+                    //                                  LoadedDevice.details_offsets[index * 3 + 2]);
+                    StartCoroutine(LerpMove(child, child.localPosition + new Vector3(LoadedDevice.details_offsets[index * 3],
                                                       LoadedDevice.details_offsets[index * 3 + 1],
-                                                      LoadedDevice.details_offsets[index * 3 + 2]);
+                                                      LoadedDevice.details_offsets[index * 3 + 2]), child.localPosition, 2f, Time.time));
                     index++;
                 }
             }
@@ -88,11 +130,17 @@ public class InterNextCore : MonoBehaviour
 
                 if (!child.CompareTag("DeviceIgnore"))
                 {
-                    child.localPosition = new Vector3();
+                    StartCoroutine(LerpMove(child, DetailsRealOffsets[index], child.localPosition, 2f, Time.time));
+                    //child.localPosition = DetailsRealOffsets[index];
                     index++;
                 }
             }
         }
         disassembled = !disassembled;
+    }
+
+    public void TogglePower()
+    {
+        PowerDevice = !PowerDevice;
     }
 }
---
+++ Assets/InterNext/InterNextSpin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterNextSpin : MonoBehaviour
+{
+    public int Axis;
+    public float Speed;
+
+    void Update()
+    {
+        if (!InterNextCore.PowerDevice)
+            return;
+        if (Axis == 0)
+            transform.Rotate(Speed, 0, 0, Space.World);
+        if (Axis == 1)
+            transform.Rotate(0, Speed, 0, Space.World);
+        if (Axis == 2)
+            transform.Rotate(0, 0, Speed, Space.World);
+    }
+}
